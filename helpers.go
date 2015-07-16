3f9c475dff4f78d8150bd9d76b16320ff6f4a651
Danny Hermes <daniel.j.hermes@gmail.com>
2015-07-15 20:57:43 -0700

    Moving client config into helpers.go

--- helpers.go
+++ helpers.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/cloud"
+	"google.golang.org/cloud/bigtable"
 )
 
 func getUseAppDefault() bool {
@@ -11,4 +18,35 @@ func getUseAppDefault() bool {
 		"Boolean to determine if app. default credentials should be used")
 	flag.Parse()
 	return useAppDefault
+}
+
+func getClientArgs() (*context.Context, *cloud.ClientOption, error) {
+	useAppDefault := getUseAppDefault()
+
+	var ctx context.Context
+	var clientOption cloud.ClientOption
+	if useAppDefault {
+		tokenSrc, err := google.DefaultTokenSource(
+			oauth2.NoContext, ScopeCloudPlatform)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		ctx = oauth2.NoContext
+		clientOption = cloud.WithTokenSource(tokenSrc)
+	} else {
+		jsonKey, err := ioutil.ReadFile(KeyFile)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		config, err := google.JWTConfigFromJSON(jsonKey, bigtable.ClusterAdminScope)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		ctx = cloud.NewContext(ProjectID, config.Client(oauth2.NoContext))
+		clientOption = cloud.WithTokenSource(config.TokenSource(ctx))
+	}
+	return &ctx, &clientOption, nil
 }
--- main_with_cluster_admin.go
+++ main_with_cluster_admin.go
@@ -2,53 +2,23 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/cloud"
 	"google.golang.org/cloud/bigtable"
 )
 
 func main() {
-	useAppDefault := getUseAppDefault()
-
-	var ctx context.Context
-	var clientOption cloud.ClientOption
-	if useAppDefault {
-		tokenSrc, err := google.DefaultTokenSource(
-			oauth2.NoContext, ScopeCloudPlatform)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		ctx = oauth2.NoContext
-		clientOption = cloud.WithTokenSource(tokenSrc)
-	} else {
-		jsonKey, err := ioutil.ReadFile(KeyFile)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		config, err := google.JWTConfigFromJSON(jsonKey, bigtable.ClusterAdminScope)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		ctx = cloud.NewContext(ProjectID, config.Client(oauth2.NoContext))
-		clientOption = cloud.WithTokenSource(config.TokenSource(ctx))
+	ctx, clientOption, err := getClientArgs()
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
 
 	client, err := bigtable.NewClusterAdminClient(
-		ctx, ProjectID, clientOption)
+		*ctx, ProjectID, *clientOption)
 
 	// Get the list of clusters.
-	clusterInfo, err := client.Clusters(ctx)
+	clusterInfo, err := client.Clusters(*ctx)
 	if err != nil {
 		log.Fatal(err)
 		return
